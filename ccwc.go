ccd147febeb5d1073f2128504dafc1683da9483c
PLCodingStuff <plproton47@proton.me>
2025-05-16 18:23:57 +0300

    Parsing
    Created main file
    Added input arg parse for `Step 1`
    Implemented file type checking

---
+++ ccwc.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func parse_dots(input_string string) [2][]byte {
+	var filename_parse [2][]byte
+	var word uint8 = 0
+	char := 0
+
+	for input_char := 0; input_char < len(input_string); input_char++ {
+		filename_parse[word] = append(filename_parse[word], input_string[input_char])
+		if input_string[input_char] == 46 {
+			word++
+			char = 0
+		}
+		char++
+	}
+
+	return filename_parse
+}
+
+func read_file(filename string) int {
+	dat, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return len(dat)
+}
+
+func compute_bytes() {
+
+}
+
+func parse_input() {
+	input_args := os.Args
+	file := input_args[len(input_args)-1]
+
+	// Check if file is `txt`
+	parsed_filename := parse_dots(file)
+	if parsed_filename[1] == nil || string(parsed_filename[1]) != "txt" {
+		fmt.Println("ERROR: Invalid file type")
+		return
+	}
+
+	// Parse flags
+	for flag := 1; flag < len(input_args)-1; flag++ {
+		if input_args[flag][0] != 45 {
+			fmt.Println("ERROR: Invalid flag")
+			return
+		}
+		if input_args[flag][1] != 'c' {
+			fmt.Println("Invalid flag")
+		}
+
+	}
+}
+
+func main() {
+	parse_input()
+}
