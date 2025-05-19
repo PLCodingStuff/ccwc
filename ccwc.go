8d95177ac3ce02b6829e3c0f77137773d1d70ee6
PLCodingStuff <plproton47@proton.me>
2025-05-19 20:47:03 +0300

    Added -l & -w
    Added functionality for line count and word count. Currently, both work, but only -l passes the test.

--- ccwc.go
+++ ccwc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"iter"
 	"os"
 )
 
@@ -49,6 +50,86 @@ func read_bytes(filename string) uint64 {
 	}
 
 	return bytes
+
+}
+
+func read_lines(filename string) uint64 {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	var lines uint64 = 0
+
+	for {
+		_, err := reader.ReadBytes('\n')
+		if err != nil {
+
+			if err.Error() == "EOF" {
+				break
+			}
+			panic(err)
+		}
+
+		lines++
+	}
+
+	return lines
+}
+
+func iterate_words(s string, sep byte) iter.Seq[string] {
+	return func(yield func(string) bool) {
+		var start, c, s_length uint64 = 0, 0, uint64(len(s))
+
+		for c = 0; c < s_length; c++ {
+			if c == s_length || s[c] == sep {
+				if !yield(s[start:c]) {
+					return
+				}
+				start = uint64(c) + 1
+			}
+		}
+	}
+}
+
+func read_words(filename string) uint64 {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	var words uint64 = 0
+
+	for {
+
+		bytes_line, err := reader.ReadBytes('\n')
+		if err != nil {
+
+			if err.Error() == "EOF" {
+				break
+			}
+			panic(err)
+		}
+		string_line := string(bytes_line)
+
+		// fmt.Println(string_line)
+		iterate_words(string_line, ' ')(func(word string) bool {
+			// fmt.Println(word)
+			words++
+			return true
+		})
+		fmt.Println()
+	}
+
+	return words
 }
 
 func parse_input() {
@@ -74,10 +155,15 @@ func parse_input() {
 			fmt.Println("ERROR: Invalid flag")
 			return
 		}
-		if input_args[flag][1] != 'c' {
-			fmt.Println("Invalid flag")
+		switch input_args[flag][1] {
+		case 'c':
+			fmt.Printf("%d", read_bytes(file))
+		case 'l':
+			fmt.Printf("%d", read_lines(file))
+		case 'w':
+			fmt.Printf("%d", read_words(file))
 		}
-		fmt.Println(read_bytes(file))
+		fmt.Printf(" %s\n", file)
 	}
 }
 
--- ccwc_test.go
+++ ccwc_test.go
@@ -8,9 +8,30 @@ const test_file string = "test.txt"
 
 func TestByteCount(t *testing.T) {
 	const expected_result uint64 = 342190
+
 	result := read_bytes(test_file)
 	if result != expected_result {
-		t.Errorf("reab_bytes(\"%s\") \"FAIL: expected result %d, got %d.\n", test_file, expected_result, result)
+		t.Errorf("read_bytes(\"%s\") FAIL: expected result %d, got %d.\n", test_file, expected_result, result)
+	}
+	t.Logf("PASS\n")
+}
+
+func TestLineCount(t *testing.T) {
+	const expected_result uint64 = 7145
+
+	result := read_lines(test_file)
+	if result != expected_result {
+		t.Errorf("read_lines(\"%s\") FAIL: expected result %d, got %d.\n", test_file, expected_result, result)
+	}
+	t.Logf("PASS\n")
+}
+
+func TestWordCount(t *testing.T) {
+	const expected_result uint64 = 58164
+
+	result := read_words(test_file)
+	if result != expected_result {
+		t.Errorf("read_word(\"%s\") FAIL: expected result %d, got %d.\n", test_file, expected_result, result)
 	}
 	t.Logf("PASS\n")
 }
